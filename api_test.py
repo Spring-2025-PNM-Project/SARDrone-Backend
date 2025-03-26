5b1f33394c83dec5ee7037928f739d6da4ec7d97
kevintsoii <kevin.tsoi44@gmail.com>
2025-03-26 03:28:27 -0700

    update requirements and test file name

--- api_test.py
+++ api_test.py
@@ -1,17 +1,10 @@
-print("I am rachel hahahha")
-print("u suck")
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app
+
 client = TestClient(app)
 
 def test_tha_status():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
-
-def myEquation(x):
-    return x + 10
-
-def test_MyEquation():
-    assert myEquation(5) == 15
