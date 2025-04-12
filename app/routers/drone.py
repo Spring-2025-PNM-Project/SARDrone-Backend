62a41f369c64cac3e0ff4c5978ba870e2500eb94
kevintsoii <kevin.tsoi44@gmail.com>
2025-04-11 20:36:35 -0700

    add websocket endpoint

--- app/routers/drone.py
+++ app/routers/drone.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from app.utils.connection_manager import ConnectionManager
 from app.schemas.drone import DroneStatus, DroneStatusResponse
 
 
@@ -8,7 +8,23 @@ router = APIRouter(
     tags=["Drone"]
 )
 
+manager = ConnectionManager()
+
+instructions = []
+
+
+@router.websocket("/ws/{drone_id}")
+async def websocket_endpoint(websocket: WebSocket, drone_id: str):
+    await manager.connect(drone_id, websocket)
+
+    try:
+        while True:
+            instruction = await websocket.receive_text()
+            instructions.append(instruction)
+    except WebSocketDisconnect:
+        manager.disconnect(drone_id, websocket)
 
 @router.post("/status", response_model=DroneStatusResponse)
 async def update_drone_status(status: DroneStatus):
-    return {"message": "Drone status updated successfully", "instructions": ["take off"]}
+    await manager.send_drone_status(status.drone_id, status.model_dump())
+    return {"instructions": instructions}
---
+++ app/utils/connection_manager.py
@@ -0,0 +1,26 @@
+from fastapi import WebSocket
+from collections import defaultdict
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: dict[str, list[WebSocket]] = defaultdict(list)
+
+    async def connect(self, drone_id: str, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections[drone_id].append(websocket)
+
+    def disconnect(self, drone_id: str, websocket: WebSocket):
+        if websocket in self.active_connections[drone_id]:
+            self.active_connections[drone_id].remove(websocket)
+            if not self.active_connections[drone_id]:
+                del self.active_connections[drone_id]
+
+    async def send_drone_status(self, drone_id: str, data: dict):
+        connections = self.active_connections.get(drone_id, [])
+        for websocket in connections:
+            try:
+                await websocket.send_json(data)
+            except Exception as e:
+                print('Error sending message:', e)
+                self.disconnect(drone_id, websocket)
