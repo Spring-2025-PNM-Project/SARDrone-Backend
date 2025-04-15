2f258ad73c34d2d39a1899f3942381125c03df75
Marvin <marvin.zhai@sjsu.edu>
2025-04-14 21:26:14 -0700

    store drone status in db

--- app/routers/drone.py
+++ app/routers/drone.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, BackgroundTasks
 from app.utils.connection_manager import ConnectionManager
 from app.schemas.drone import DroneStatus, DroneStatusResponse
+from app.services.database import get_database
 from collections import defaultdict, deque
-
+from datetime import datetime, timezone
 
 router = APIRouter(
     prefix="/drone",
@@ -13,6 +14,16 @@ manager = ConnectionManager()
 
 instructions = defaultdict(deque)
 
+db = get_database()
+
+async def save_drone_status(status: dict):
+    try:
+
+        status["created_at"] = datetime.now(timezone.utc)    
+        db["logs"].insert_one(status)
+        
+    except Exception as e:
+        print("Error saving drone status:", e)
 
 @router.websocket("/ws/{drone_id}")
 async def websocket_endpoint(websocket: WebSocket, drone_id: str):
@@ -26,8 +37,10 @@ async def websocket_endpoint(websocket: WebSocket, drone_id: str):
         manager.disconnect(drone_id, websocket)
 
 @router.post("/status", response_model=DroneStatusResponse)
-async def update_drone_status(status: DroneStatus):
+async def update_drone_status(status: DroneStatus, background_tasks: BackgroundTasks):
     await manager.send_drone_status(status.drone_id, status.model_dump(exclude_none=True))
+
+    background_tasks.add_task(save_drone_status, status.model_dump(exclude_none=True))
 
     drone_instructions = []
     instructions_queue = instructions[status.drone_id]
@@ -35,4 +48,5 @@ async def update_drone_status(status: DroneStatus):
     while instructions_queue:
         drone_instructions.append(instructions_queue.popleft())
 
+    print(f"Returning instructions for drone {status.drone_id}: {drone_instructions}")
     return {"instructions": drone_instructions}
