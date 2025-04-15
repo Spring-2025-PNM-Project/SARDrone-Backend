62c28b43172c8a8bb308e668ac793174dce67bab
Jomi Angelina Arockiasamy <122714247+jomiarockiasamy@users.noreply.github.com>
2025-04-14 17:30:52 -0700

    Update classification.py

--- app/services/classification.py
+++ app/services/classification.py
@@ -1,30 +1,65 @@
-import base64
 import os
+import json
+import PIL.Image
+
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types
-import PIL.Image
 from io import BytesIO
+
 
 load_dotenv()
 
+
+GEMINI_MODEL = "gemini-2.0-flash"
+PROMPT = '''
+You are an AI vision model deployed on a Search and Rescue (SAR) drone.
+
+Task:
+Analyze the provided image and detect any humans.
+
+Important Constraints:
+Only detect humans.
+
+
+Return output strictly in the JSON format provided. Do not add any extra commentary or text outside the JSON.
+Bounding boxes must follow this exact format: [ymin, xmin, ymax, xmax] with integers between 0 and 1000.
+
+JSON Response Format:
+{
+  "text_description": "Brief summary of the scene and any humans detected.",
+  "confidence_score": integer from 1 to 100 indicating confidence a human is present,
+  "bounding_boxes": [
+    [ymin, xmin, ymax, xmax],
+    ...
+  ]
+}
+
+If no humans are detected, set confidence_score to 0 and leave bounding_boxes as an empty list.
+'''
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+
 class ClassificationModel:
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
     @staticmethod
     def generate(image_bytes: bytes):
         try:
             image = PIL.Image.open(BytesIO(image_bytes))
-            client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-            response = client.models.generate_content(
-                model  ="gemini-2.0-flash",
-                contents = ["Classify whether or not there is a person in the image with a score of 0 - 100", image]
-            )
+            response = ClassificationModel.client.models.generate_content(
+                model = GEMINI_MODEL,
+                contents = [image, PROMPT]
+            ).text
             
-            print(response.text)
-            return response.text
+            lines = response.splitlines()
+            for i, line in enumerate(lines):
+                if line == "```json":
+                    response = "\n".join(lines[i+1:])
+                    response = response.split("```")[0]
+                    break
+            
+            return json.loads(response)
         except Exception as e:
-            print(e)
-            print("An exception has occured with generating response")
-
-    if __name__ == "__main__":
-        generate()
+            print("Gemini Error", e)
