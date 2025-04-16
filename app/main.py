d3de062cd6e288086819077480d74c5db1852d9a
kevintsoii <kevin.tsoi44@gmail.com>
2025-04-15 19:34:46 -0700

    modify endpoint names

--- app/main.py
+++ app/main.py
@@ -13,16 +13,7 @@ classificationmodel = ClassificationModel()
 
 @app.get('/')
 def root():
-    return {"Hello": "World"}
-
-@app.post("/upload/")
-async def process_image(file: UploadFile = File(...)):
-    try:
-        image_bytes = await file.read()
-        result = classificationmodel.generate(image_bytes)
-        return {"result": result}
-    except Exception as e:
-        return {"message": e.args}
+    return {"status": "ok"}
 
 
 if __name__ == "__main__":
