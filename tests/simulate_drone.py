88557b7126edabd23b2054f6b5f9aec1249c30d6
kevintsoii <kevin.tsoi44@gmail.com>
2025-04-23 04:23:49 -0700

    source from test images for drone simulation

--- tests/simulate_drone.py
+++ tests/simulate_drone.py
@@ -2,6 +2,10 @@ import asyncio
 import base64
 import requests
 import time
+import os
+import random 
+from PIL import Image
+import io
 
 from app.main import app
 
@@ -14,6 +18,12 @@ drone_status = {
 }
 
 
+yes_human_folder = "images/yes_human"
+no_human_folder = "images/no_human"
+
+images = [os.path.join(yes_human_folder, f) for f in os.listdir(yes_human_folder)]
+images.extend([os.path.join(no_human_folder, f) for f in os.listdir(no_human_folder)])
+
 # Drone behavior simulation
 async def simulate_drone():
     # Simulate drone status updates
@@ -23,7 +33,9 @@ async def simulate_drone():
         i += 1
         drone_status["timestamp"] = int(time.time())
 
-        response = requests.post(f"https://api.meritdrone.site/drone/{drone_id}/info", json=drone_status).json()
+        response = requests.post(f"https://api.meritdrone.site/drone/{drone_id}/info", json=drone_status)
+
+        response = response.json()
         status_for_logging = drone_status.copy()
         if "image" in status_for_logging:
             status_for_logging["image"] = status_for_logging["image"][:10]
@@ -36,11 +48,23 @@ async def simulate_drone():
 
             if instruction == "takeoff":
                 drone_status["status"] = "flying"
+                
+                file_path = random.choice(images)
 
-                file_path = "tests/images/droneimage1.jpg"
-                with open(file_path, "rb") as f:
-                    image_bytes = f.read()
-                    encoded_image = base64.b64encode(image_bytes).decode("utf-8")
+                with Image.open(file_path) as img:
+                    # Resize to max 640px width (keep aspect ratio)
+                    img.thumbnail((640, 640))
+
+                    # Save to a bytes buffer in JPEG format with reduced quality
+                    buffer = io.BytesIO()
+                    img.save(buffer, format="JPEG", quality=70)  # quality 0-100
+                    buffer.seek(0)
+
+                    # Encode to base64
+                    encoded_image = base64.b64encode(buffer.read()).decode("utf-8")
+
+
+
                 drone_status["image"] = encoded_image
             elif instruction == "shutdown":
                 drone_status["status"] = "shutdown"
